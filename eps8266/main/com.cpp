bde5e27f434d7cbaa8978554157448827a6a406b
arubiog <arubio@gmx.es>
2017-04-14 21:15:24 +0200

    Add files via upload

---
+++ eps8266/main/com.cpp
@@ -0,0 +1,82 @@
+#include "com.h"
+
+WifiCom::WifiCom(){
+  serverClient = WiFiClient();
+}
+
+WifiCom::WifiCom(WiFiClient &other){
+  serverClient = WiFiClient(other);
+}
+
+bool WifiCom::isConnected(){
+  return serverClient.connected();
+}
+
+bool WifiCom::readCmd(){
+  int index = 0;
+  char inChar;
+  bool newCmd = false;
+  while(serverClient.available() > 0){
+    newCmd = true;
+    if(index < MAX_READ_BUFFER){
+      inChar = serverClient.read(); // Read a character
+      if (inChar == '\n' or inChar == '\r'){
+        readBuffer[index] = '\0';
+        break;
+      }
+      readBuffer[index] = inChar; // Store it
+      index++; // Increment where to write next
+      readBuffer[index] = '\0'; // Null terminate the string
+    }
+    else{
+      newCmd = false;
+      serverClient.write("Command too long.\n");
+    }
+  }
+  if(index > 0 and newCmd and !isCmdValid()){
+       newCmd = false;
+       serverClient.write("Wrong command\n");
+  }
+  return newCmd;
+}
+
+Cmds WifiCom::getCommand(){
+  if (!readCmd()){
+    readBuffer[0]='\0';
+    cmd = NONE;
+  }
+  return cmd;
+}
+
+WifiCom& WifiCom::operator=(WiFiClient& other){
+  serverClient = other;
+  return *this;
+}
+bool WifiCom::isCmdValid(){
+  String str(readBuffer);
+  str.toUpperCase();
+  
+  //TODO Implement the protocol
+  if (str == "TEMP")
+    cmd = TEMP;
+  else if (str == "STATE")
+    cmd = STATE;
+  else if(str == "CONFIG")
+    cmd = CONFIG;
+  else if(str == "DI")
+    cmd = DI;
+  else if(str == "DO")
+    cmd = DO;
+  else{
+    cmd = NONE;
+  }
+  return cmd==NONE? false: true;
+}
+
+void WifiCom::sendData(String data){
+  serverClient.write(data.c_str());
+}
+
+
+
+
---
+++ eps8266/test01.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import socket
+
+class RemoteSensor(object):
+    def __init__(self, host, port=23, timeout=3):
+        self.rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.rs.settimeout(timeout)
+        self.rs.connect((host,port))
+    def __delete__(self):
+        self.rs.close()
+
+    def _sendCmd(self, cmd, size=8096):
+        while len(cmd):
+            n = self.rs.send(cmd)
+            cmd = cmd[n:]
+        answer=""
+        while True:
+            answer = self.rs.recv(size)
+            if answer[-1] == '\n':
+                break
+        return answer[:-1]
+            
+    def getTemp(self):
+        values = self._sendCmd('temp').split(';')[:-1]
+        return values
+
+    def getStates(self):
+        values = self._sendCmd('state').split(';')[:-1]
+        return values
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        host = sys.argv[1]
+    else:
+        host = '10.210.1.135'
+    rs = RemoteSensor(host)
+    for i in range(20):
+        print rs.getTemp() 
+        print rs.getStates()
+        
+    
