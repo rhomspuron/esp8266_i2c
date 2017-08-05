9584cf4a7ed903ce089d835d76ba1ad03762c7a7
Roberto Javier Homs Puron <rhoms@cells.es>
2017-08-05 14:13:36 -0700

    Include license

--- firmware/main/max31855.cpp
+++ firmware/main/max31855.cpp
@@ -1,3 +1,28 @@
+
+/*********************************************************************
+Arduino project for a remote temperature sensor base on ESP8266. 
+
+Autors: Roberto J. Homs Puron <rhoms@cells.es>, 
+        Alberto Rubio Garcia <arubio@cells.es>
+        Sergio Astorga Sanchez <sastorga@cells.es>
+        
+Copyright (C) 2017, CELLS / ALBA Synchrotron
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+**************************************************************/
+
 #include "max31855.h"
 
 MAX31855::MAX31855(int8_t cs, int8_t sck, int8_t miso):
